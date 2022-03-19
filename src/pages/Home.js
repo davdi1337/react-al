718f0d6d9639b01c091669038f1279ddbcd5f6dc
babailan <ronnelbabailan@outlook.com>
2022-03-20 00:50:11 +0800

    fix setting up a localstorage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,51 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { SearchContext } from '../context/search';
-import { FaSearch } from 'react-icons/fa'
-import { Input, IconButton, Grid, InputGroup, InputRightElement } from '@chakra-ui/react';
+import { SearchContext } from "../context/search";
+import { FaSearch } from "react-icons/fa";
+import {
+  Input,
+  IconButton,
+  Grid,
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react";
 
 const Home = () => {
-    const search = useContext(SearchContext)
-    const [input, setInput] = useState('')
-    const navigate = useNavigate()
+  const search = useContext(SearchContext);
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
 
-    const handleSearch = (event) => {
-        event.preventDefault()
-        search.search(input).then((data) => {
-            console.log(data)
-            search.setData(data.results);
-            localStorage.setItem('myData', JSON.stringify(data.results));
-            navigate('/results');
-        });
-    };
-    return (
-        <Grid flexDirection="column" justifyContent="center" alignItems="center">
-            <Grid>
-                <Grid>
-                <form onSubmit={handleSearch}>
-                <InputGroup>
-                <Input placeholder="Search anime" onChange={(event) => setInput(event.target.value)}/>
-                <InputRightElement>
-                    <IconButton icon={<FaSearch/>} onClick={handleSearch} size="sm"></IconButton>
-                </InputRightElement>
-                </InputGroup>
-                </form>
-                </Grid>
-            </Grid>
+  const handleSearch = (event) => {
+    event.preventDefault();
+    search.search(input).then((res) => {
+      search.setData(res.data);
+      localStorage.setItem("myData", JSON.stringify(res.data));
+      navigate("/results");
+    });
+  };
+  return (
+    <Grid flexDirection="column" justifyContent="center" alignItems="center">
+      <Grid>
+        <Grid>
+          <form onSubmit={handleSearch}>
+            <InputGroup>
+              <Input
+                placeholder="Search anime"
+                onChange={(event) => setInput(event.target.value)}
+              />
+              <InputRightElement>
+                <IconButton
+                  icon={<FaSearch />}
+                  onClick={handleSearch}
+                  size="sm"
+                ></IconButton>
+              </InputRightElement>
+            </InputGroup>
+          </form>
         </Grid>
-    );
+      </Grid>
+    </Grid>
+  );
 };
 
-export default Home
+export default Home;
--- src/pages/Results.js
+++ src/pages/Results.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useContext, useState } from 'react'
-import { SearchContext } from '../context/search'
+import React, { useEffect, useContext, useState } from "react";
+import { SearchContext } from "../context/search";
 
 const Results = () => {
-  const search = useContext(SearchContext)
-  const [dataExists, setDataExists] = useState(true)
+  const search = useContext(SearchContext);
+  const [dataExists, setDataExists] = useState(true);
   useEffect(() => {
     if (search.animeData === undefined || search.animeData.length === 0) {
       try {
-        search.setData(JSON.parse(localStorage.getItem('myData')));
+        search.setData(JSON.parse(localStorage.getItem("myData")));
         setDataExists(true);
       } catch (error) {
         console.log(error);
@@ -15,11 +15,7 @@ const Results = () => {
       }
     }
   }, [search]);
-  return (
-    <div>
-      {(dataExists && 'Data Exists' || 'Data does not exists')}
-    </div>
-  )
-}
+  return <div>{(dataExists && "Data Exist") || "Data does not exists"}</div>;
+};
 
-export default Results
+export default Results;
